1d206262747aa29a146ed5ec5e824435f41fcd23
msanchezromero <104275698+msanchezromero@users.noreply.github.com>
2025-07-01 17:46:10 +0200

    Create 00_main.jl

---
+++ src/00_main.jl
@@ -0,0 +1,35 @@
+using Plots
+using Distributed
+using DataFrames
+using ColorSchemes
+using StatsPlots
+using GLM
+using MixedModels
+using CSV
+using JLD
+
+using Random, LinearAlgebra, StatsBase
+
+display(CaseScenario)
+# ----- Uncomment for parallelizing using Distributed ------
+addprocs((CaseScenario==1) ? 2 : 0)
+# ------- Uncomment for parallelizing using Threads --------
+#println(Threads.nthreads()) 
+# ----------------------------------------------------------
+display("Number of processors: "*string(nprocs()))
+
+@everywhere path    = "../"
+
+@everywhere include(path*"src/01_dictionaries.jl");
+@everywhere include(path*"src/02a_param_struct.jl");
+@everywhere include(path*"src/02b_parameters.jl");
+@everywhere include(path*"src/02c_parameter_functions.jl");
+
+@everywhere include(path*"src/03_init_parallel.jl");
+@everywhere include(path*"src/04_functions_micro.jl");
+@everywhere include(path*"src/05_equilibrium_prices.jl");
+@everywhere include(path*"src/06_plots_benchmark.jl");
+@everywhere include(path*"src/07_database.jl");
+
+
+
